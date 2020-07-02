6611706e93b3410a476ef6ab53e81e9912276ea8
Sandeep Dilip <sanman1k98@gmail.com>
2020-07-01 22:53:11 -0400

    Initial Commit

---
+++ Widget Player/Widget_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Widget_PlayerApp.swift
+//  Widget Player
+//
+//  Created by Sandeep Dilip on 7/1/20.
+//
+
+import SwiftUI
+
+@main
+struct Widget_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
