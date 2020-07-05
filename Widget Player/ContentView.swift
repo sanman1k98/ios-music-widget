7e660485b98b0ca6cf5211e6dd629b71a0dd923f
Sandeep Dilip <sanman1k98@gmail.com>
2020-07-04 21:08:39 -0400

    Combine Play and Pause Buttons and extract closure as func

--- Widget Player/ContentView.swift
+++ Widget Player/ContentView.swift
@@ -10,39 +10,37 @@ import MediaPlayer
 
 
 struct ContentView: View {
-    @State private var playing = false
     let mediaController = MPMusicPlayerController.systemMusicPlayer
 
     var body: some View {
         
         VStack {
             Rectangle()
-                .fill(playing ? Color.white : Color.gray)
+                .fill(mediaController.playbackState == .playing ? Color.white : Color.gray)
                 .frame(width: 250, height: 250)
                 .padding(.top, 50)
             
             Spacer()
             
-            HStack {
-                Button("Play") {
-                    withAnimation {
-                        playing = true
-                        mediaController.play()
-                    }
-                }
-                Button("Pause") {
-                    withAnimation {
-                        playing = false
-                        mediaController.pause()
-                    }
+            Button("Play/Pause") {
+                withAnimation {
+                    playPause()
                 }
             }
-            
             Spacer()
         }
     }
     
-    
+    private func playPause() {
+        if mediaController.playbackState == .playing {
+            mediaController.pause()
+        } else if !mediaController.isPreparedToPlay {
+            mediaController.prepareToPlay()
+            mediaController.play()
+        } else {
+            mediaController.play()
+        }
+    }
     
 }
 
