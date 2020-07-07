a15bfc2e574f0182c2fb439aa39a703ae1fd4343
Sandeep Dilip <sanman1k98@gmail.com>
2020-07-07 19:09:43 -0400

    Restructure ContentView, using logging again

--- Widget Player/ContentView.swift
+++ Widget Player/ContentView.swift
@@ -7,50 +7,75 @@
 
 import SwiftUI
 import MediaPlayer
+import os
 
+let mediaController = MPMusicPlayerController.systemMusicPlayer
+let logger = Logger(subsystem: "com.deepstuff.Widget-Player", category: "Debug")
 
 struct ContentView: View {
-    let mediaController = MPMusicPlayerController.systemMusicPlayer
-
     var body: some View {
-        
         VStack {
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 250, height: 250)
-                .padding(.top, 50)
-            
+            Artwork()
+                .padding(.top, 30)
             Spacer()
-            
-            Button(action: {
-                withAnimation {
-                    playPause()
-                }
-            }, label: {
-                Label("Play/Pause", systemImage: "playpause")
-                    .labelStyle(IconOnlyLabelStyle())
-                    .font(.largeTitle)
-            })
-            
+            Controls()
             Spacer()
         }
     }
     
-    private func playPause() {
-        switch mediaController.playbackState {
-        case .playing:
-            mediaController.pause()
-        case .paused, .stopped, .interrupted:
-            mediaController.play()
-        default:
-            mediaController.play()
-        }
+    
+}
+
+struct Artwork: View {
+    var body: some View {
+        Image(uiImage: (currentArtwork() ?? UIImage(systemName: "music.note"))! )
+            .resizable(capInsets: EdgeInsets(), resizingMode: .stretch)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 300, height: 300)
+    }
+    
+    private func currentArtwork() -> UIImage? {
+        var artwork :UIImage?
+        artwork = mediaController.nowPlayingItem?.artwork?.image(at: CGSize(width: 300, height: 300))
+        logger.log("\(artwork.debugDescription)")
+        return artwork
     }
     
 }
 
+struct Controls: View {
+    var body: some View {
+        Button(action: {
+            withAnimation {
+                playPause()
+            }
+        }, label: {
+            Label("Play/Pause", systemImage: "playpause")
+                .labelStyle(IconOnlyLabelStyle())
+                .font(.largeTitle)
+        })
+    }
+    
+    private func playPause() {
+            switch mediaController.playbackState {
+            case .playing:
+                mediaController.pause()
+            case .paused, .stopped, .interrupted:
+                mediaController.play()
+            default:
+                mediaController.play()
+            }
+        }
+
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.dark)
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+            Artwork()
+                .previewLayout(.sizeThatFits)
+        }
     }
 }
