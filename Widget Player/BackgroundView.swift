fed50150093f1ef3b34738fb7a0244ae1796b970
Sandeep Dilip <sanman1k98@gmail.com>
2020-09-25 18:53:38 -0400

    Add two new views

---
+++ Widget Player/BackgroundView.swift
@@ -0,0 +1,21 @@
+//
+//  BackgroundView.swift
+//  Widget Player
+//
+//  Created by Sandeep Dilip on 9/25/20.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ Widget Player/InfoView.swift
@@ -0,0 +1,21 @@
+//
+//  InfoView.swift
+//  Widget Player
+//
+//  Created by Sandeep Dilip on 9/25/20.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
